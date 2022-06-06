e5c123f9fe1a3a5d3acc34fbc0658e057fd96649
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-06 18:20:11 +0300

    Completed the lab.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let cost;
+  if(ride<=400){
+    cost = 'This one is on me!'
+  }else if(ride > 2000 && ride <= 2500){
+    cost ='I will gladly take your thirty bucks.';
+  }else if(ride > 2500){
+   cost = 'No can do.';
+  }
+  return cost;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+const validation = city==='NYC'? 'Ok, sounds good.':'No go.';
+return validation;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(charmfromTip){
   // Write your code here!
+switch(charmfromTip){
+   case 'generous':
+     return 'Thank you so much.';
+   case 'not as generous':
+     return 'Thank you.';
+   default :
+     return 'Bye.';
+}
 }
